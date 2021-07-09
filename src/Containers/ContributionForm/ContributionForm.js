8e3dfafc464eafab9760705ab32ddc3bca8090c9
Riley Martin <riley.willow@gmail.com>
2021-07-09 06:42:05 -0600

    Write handleChange method

--- src/Containers/ContributionForm/ContributionForm.js
+++ src/Containers/ContributionForm/ContributionForm.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 
-class Form extends Component {
+class ContributionForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -13,13 +13,26 @@ class Form extends Component {
     }
   }
 
+  handleChange = (event) => {
+    this.setState({ [event.targetname]: event.target.value })
+  }
+
   render() {
     return (
       <>
         <form>
           <h2>Contribution Form</h2>
+          <input
+          type='text'
+          placeholder='First Name'
+          name='firstName'
+          value={this.state.firstName}
+          onChange={event => this.handleChange(event)}
+        />
         </form>
       </>
     )
   }
 }
+
+export default ContributionForm
