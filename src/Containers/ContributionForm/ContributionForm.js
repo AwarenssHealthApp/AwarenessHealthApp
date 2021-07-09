580c18ba8f4f57bb770378cabb3209855cc70531
Riley Martin <riley.willow@gmail.com>
2021-07-09 06:44:38 -0600

    Add inputs for form field

--- src/Containers/ContributionForm/ContributionForm.js
+++ src/Containers/ContributionForm/ContributionForm.js
@@ -23,12 +23,47 @@ class ContributionForm extends Component {
         <form>
           <h2>Contribution Form</h2>
           <input
-          type='text'
-          placeholder='First Name'
-          name='firstName'
-          value={this.state.firstName}
-          onChange={event => this.handleChange(event)}
-        />
+            type='text'
+            placeholder='First Name'
+            name='firstName'
+            value={this.state.firstName}
+            onChange={event => this.handleChange(event)}
+          />
+          <input
+            type='text'
+            placeholder='Last Name'
+            name='lastName'
+            value={this.state.lastName}
+            onChange={event => this.handleChange(event)}
+          />
+          <input
+            type='text'
+            placeholder='Specialty'
+            name='specialty'
+            value={this.state.specialty}
+            onChange={event => this.handleChange(event)}
+          />
+          <input
+            type='text'
+            placeholder='Insurance'
+            name='insurance'
+            value={this.state.insurance}
+            onChange={event => this.handleChange(event)}
+          />
+          <input
+            type='text'
+            placeholder='Address'
+            name='address'
+            value={this.state.address}
+            onChange={event => this.handleChange(event)}
+          />
+          <input
+            type='text'
+            placeholder='Phone Number'
+            name='phoneNum'
+            value={this.state.phoneNum}
+            onChange={event => this.handleChange(event)}
+          />
         </form>
       </>
     )
