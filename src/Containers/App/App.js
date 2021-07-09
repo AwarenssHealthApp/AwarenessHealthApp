5c13f947da136e913a54695118473cbdcab3aa68
Riley Martin <riley.willow@gmail.com>
2021-07-09 07:26:26 -0600

    Connect ContributionForm to App

--- src/Containers/App/App.js
+++ src/Containers/App/App.js
@@ -1,10 +1,13 @@
+import React from 'react'
 import './App.css';
+import ContributionForm from '../ContributionForm/ContributionForm'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
       </header>
+      <ContributionForm />
     </div>
   );
 }
