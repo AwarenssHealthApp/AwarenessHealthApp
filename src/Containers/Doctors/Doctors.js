7e7401eb40ebc9b7811037f6209a0ece07c80ba2
Marceline Ball <marchettiball@gmail.com>
2021-07-09 20:19:29 -0600

    Import mockData for doctors into Doctors component

--- src/Containers/Doctors/Doctors.js
+++ src/Containers/Doctors/Doctors.js
@@ -1,9 +1,12 @@
 import React, { useState } from 'react'
+import doctors from '../../mockData'
 
 const Doctors () => {
-  const [doctorList, setDoctorList] = useSate('')
+  const [doctorList, setDoctorList] = useSate(doctors)
+  return(
+    <h1>Doctors here</h1>
+  )
 
-  
 }
 
 export default Doctors
