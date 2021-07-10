2aebd709e37678de2b38a09fb85d42c4b5aeca39
Marceline Ball <marchettiball@gmail.com>
2021-07-09 20:10:12 -0600

    Create mock data file with doctor data

---
+++ src/mockData.js
@@ -0,0 +1,77 @@
+const doctors = [
+  {
+    "type": "doctors",
+    "id": "1",
+    "attributes": {
+      "first_name": "Jennifer",
+      "last_name": "Hyer",
+      "street": "790 Delaware St.",
+      "unit": "Pavilion C",
+      "city": "Denver",
+      "state": "CO",
+      "zip": "80204",
+      "phone": "3034366000",
+      "vetted": "false",
+      "specialty": [
+        "OBGYN, Surgery"
+      ],
+      "insurance": [
+        "Kaiser Permanente"
+      ]
+    }
+  },
+  {
+    "type": "doctors",
+    "id": "2",
+    "attributes": {
+      "first_name": "Rory",
+      "last_name": "Moore",
+      "street": "2045 Franklin St",
+      "unit": "200",
+      "city": "Denver",
+      "state": "CO",
+      "zip": "80205",
+      "phone": "3033384545",
+      "vetted": "false",
+      "specialty": [
+        "OBGYN"
+      ],
+      "insurance": [
+        "Kaiser Permanente",
+        "Aetna", "Coventry",
+        "Medicare Traditional"
+      ]
+    }
+  },
+  {
+    "type": "doctors",
+    "id": "3",
+    "attributes": {
+      "first_name": "Anna",
+      "last_name": "Wegleitner",
+      "street": "950 E Harvard Ave",
+      "unit": "200",
+      "city": "Denver",
+      "state": "CO",
+      "zip": "80210",
+      "phone": "3036493200",
+      "vetted": "false",
+      "specialty": [
+        "Family Medicine", "WPATH"
+      ],
+      "insurance": [
+        "Kaiser Permanente",
+        "Aetna", "Coventry",
+        "Medicare Traditional",
+        "Anthem", "Blue Cross BlueShield National",
+        "Blue Cross and Blue Shield of Nebraska",
+        "Bright Health", "Cigna", "Cigna HealthSpring",
+        "Colorado Access", "Denver Health", "First Health",
+        "Friday Health Plans", "Health First Colorado",
+        "Humana", "Medicare Traditional", "Multiplan",
+        "Oxford Health Plans", "Rocky Mountain Health Plans",
+        "UnitedHealthcare"
+      ]
+    }
+  }
+]
