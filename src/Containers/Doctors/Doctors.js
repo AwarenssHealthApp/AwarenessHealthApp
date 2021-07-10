f51eb6d6d17ea8067b9ec004804185290bd9144a
Marceline Ball <marchettiball@gmail.com>
2021-07-09 20:47:17 -0600

    Add allDoctors variable to pass to Card component

--- src/Containers/Doctors/Doctors.js
+++ src/Containers/Doctors/Doctors.js
@@ -3,6 +3,25 @@ import { doctors } from '../../mockData'
 
 const Doctors = () => {
   const [doctorList, setDoctorList] = useState(doctors)
+  const allDoctors = doctorList.map(doctor => {
+    return(
+      <Card
+      id={doctor.id}
+      key={doctor.id}
+      firstName={doctor.attributes.first_name}
+      lastName={doctor.attributes.last_name}
+      street={doctor.attributes.street}
+      unit={doctor.attributes.unit}
+      city={doctor.attributes.city}
+      state={doctor.attributes.state}
+      zip={doctor.attributes.zip}
+      phone={doctor.attributes.phone}
+      vetted={doctor.attributes.vetted}
+      specialty={doctor.attributes.specialty}
+      insurance={doctor.attributes.insurance}
+      />
+    )
+  })
   return(
     <h1>Doctors here</h1>
   )
