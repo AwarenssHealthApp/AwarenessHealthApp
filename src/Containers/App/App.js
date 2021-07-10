436b03ba82b823a5a96c979d806e4a122ba36a2e
Marceline Ball <marchettiball@gmail.com>
2021-07-10 10:52:03 -0600

    Reformat links to appear on all pages

--- src/Containers/App/App.js
+++ src/Containers/App/App.js
@@ -11,43 +11,41 @@ function App() {
       <header className="App-header">
         <h1>Head to Toe</h1>
         <nav>
-          <Switch>
-            <Route path='/submit' render={() => {
-              return(
-                <ContributionForm />
-              )
-            }} />
-            <Route path='/doctors' render={() => {
-              return(
-                <Doctors />
-              )
-            }} />
-            <Route path='/mental_health_professionals' render={() => {
-              return(
-                <MentalHealthPros />
-              )
-            }} />
-            <Route exact path ='/' render={() => {
-              return(
-                <>
-                  <Link to='/doctors'>
-                    <button id='doctors-button'>Doctors</button>
-                  </Link>
-                  <Link to='/mental_health_professionals'>
-                    <button id='mental-health-pros-button'>Mental Health Professionals</button>
-                  </Link>
-                  <Link to='/submit'>
-                    <button id='submit-button'>Submit</button>
-                  </Link>
-                  <main>
-                    <p>A place to find queer affirming support</p>
-                  </main>
-                </>
-              )
-            }} />
-          </Switch>
+          <Link to='/doctors'>
+            <button id='doctors-button'>Doctors</button>
+          </Link>
+          <Link to='/mental_health_professionals'>
+            <button id='mental-health-pros-button'>Mental Health Professionals</button>
+          </Link>
+          <Link to='/submit'>
+            <button id='submit-button'>Submit</button>
+          </Link>
         </nav>
       </header>
+      <Switch>
+        <Route path='/submit' render={() => {
+          return(
+            <ContributionForm />
+          )
+        }} />
+        <Route path='/doctors' render={() => {
+          return(
+            <Doctors />
+          )
+        }} />
+        <Route path='/mental_health_professionals' render={() => {
+          return(
+            <MentalHealthPros />
+          )
+        }} />
+        <Route exact path ='/' render={() => {
+          return(
+            <main>
+              <p>A place to find queer affirming support</p>
+            </main>
+          )
+        }} />
+      </Switch>
     </div>
   );
 }
