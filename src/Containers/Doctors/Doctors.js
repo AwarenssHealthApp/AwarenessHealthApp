405a2529bd5177fa784329738367ab011c5b3b92
Marceline Ball <marchettiball@gmail.com>
2021-07-10 22:36:37 -0600

    Comment out code that is not fully connected so the ci tests can pass

--- src/Containers/Doctors/Doctors.js
+++ src/Containers/Doctors/Doctors.js
@@ -3,26 +3,26 @@ import { doctors } from '../../mockData'
 import Cards from '../../Components/Cards/Cards'
 
 const Doctors = () => {
-  const [doctorList, setDoctorList] = useState(doctors.attributes.list)
-  const allDoctors = doctorList.map(doctor => {
-    return(
-      <Cards
-      id={doctor.id}
-      key={doctor.id}
-      firstName={doctor.first_name}
-      lastName={doctor.last_name}
-      street={doctor.street}
-      unit={doctor.unit}
-      city={doctor.city}
-      state={doctor.state}
-      zip={doctor.zip}
-      phone={doctor.phone}
-      vetted={doctor.vetted}
-      specialties={doctor.specialties}
-      insurances={doctor.insurances}
-      />
-    )
-  })
+  // const [doctorList, setDoctorList] = useState(doctors.attributes.list)
+  // const allDoctors = doctorList.map(doctor => {
+  //   return(
+  //     <Cards
+  //     id={doctor.id}
+  //     key={doctor.id}
+  //     firstName={doctor.first_name}
+  //     lastName={doctor.last_name}
+  //     street={doctor.street}
+  //     unit={doctor.unit}
+  //     city={doctor.city}
+  //     state={doctor.state}
+  //     zip={doctor.zip}
+  //     phone={doctor.phone}
+  //     vetted={doctor.vetted}
+  //     specialties={doctor.specialties}
+  //     insurances={doctor.insurances}
+  //     />
+  //   )
+  // })
   return(
     <h2>Doctors here</h2>
   )
--- src/Containers/MentalHealthPros/MentalHealthPros.js
+++ src/Containers/MentalHealthPros/MentalHealthPros.js
@@ -3,27 +3,27 @@ import { mentalHealthProfessionals } from '../../mockData'
 import Cards from '../../Components/Cards/Cards'
 
 const MentalHealthPros = () => {
-  const [mentalHealthProsList, setMentalHealthProsList] = useState(mentalHealthProfessionals.attributes.list)
-  const allMentalHealthPros = mentalHealthProsList.map(mentalHealthPro => {
-    return(
-      <Cards
-      id={mentalHealthPro.id}
-      key={mentalHealthPro.id}
-      firstName={mentalHealthPro.first_name}
-      lastName={mentalHealthPro.last_name}
-      street={mentalHealthPro.street}
-      unit={mentalHealthPro.unit}
-      city={mentalHealthPro.city}
-      state={mentalHealthPro.state}
-      zip={mentalHealthPro.zip}
-      phone={mentalHealthPro.phone}
-      vetted={mentalHealthPro.vetted}
-      specialties={mentalHealthPro.specialties}
-      insurances={mentalHealthPro.insurances}
-      cost={mentalHealthPro.cost}
-      />
-    )
-  })
+  // const [mentalHealthProsList, setMentalHealthProsList] = useState(mentalHealthProfessionals.attributes.list)
+  // const allMentalHealthPros = mentalHealthProsList.map(mentalHealthPro => {
+  //   return(
+  //     <Cards
+  //     id={mentalHealthPro.id}
+  //     key={mentalHealthPro.id}
+  //     firstName={mentalHealthPro.first_name}
+  //     lastName={mentalHealthPro.last_name}
+  //     street={mentalHealthPro.street}
+  //     unit={mentalHealthPro.unit}
+  //     city={mentalHealthPro.city}
+  //     state={mentalHealthPro.state}
+  //     zip={mentalHealthPro.zip}
+  //     phone={mentalHealthPro.phone}
+  //     vetted={mentalHealthPro.vetted}
+  //     specialties={mentalHealthPro.specialties}
+  //     insurances={mentalHealthPro.insurances}
+  //     cost={mentalHealthPro.cost}
+  //     />
+  //   )
+  // })
   return(
     <h2>Mental health professionals here</h2>
   )
