57c0ab5b4f96ffda6a002aa3411aab0c29c1e327
Marceline Ball <marchettiball@gmail.com>
2021-07-10 21:14:25 -0600

    Refactor doctor component to take in new data format

--- src/Containers/Doctors/Doctors.js
+++ src/Containers/Doctors/Doctors.js
@@ -3,23 +3,23 @@ import { doctors } from '../../mockData'
 import Cards from '../../Components/Cards/Cards'
 
 const Doctors = () => {
-  const [doctorList, setDoctorList] = useState(doctors)
+  const [doctorList, setDoctorList] = useState(doctors.attributes.list)
   const allDoctors = doctorList.map(doctor => {
     return(
       <Cards
       id={doctor.id}
       key={doctor.id}
-      firstName={doctor.attributes.first_name}
-      lastName={doctor.attributes.last_name}
-      street={doctor.attributes.street}
-      unit={doctor.attributes.unit}
-      city={doctor.attributes.city}
-      state={doctor.attributes.state}
-      zip={doctor.attributes.zip}
-      phone={doctor.attributes.phone}
-      vetted={doctor.attributes.vetted}
-      specialty={doctor.attributes.specialty}
-      insurance={doctor.attributes.insurance}
+      firstName={doctor.first_name}
+      lastName={doctor.last_name}
+      street={doctor.street}
+      unit={doctor.unit}
+      city={doctor.city}
+      state={doctor.state}
+      zip={doctor.zip}
+      phone={doctor.phone}
+      vetted={doctor.vetted}
+      specialties={doctor.specialties}
+      insurances={doctor.insurances}
       />
     )
   })
