b2941080735afa53c51281f919307e02e2b2cf0b
Angela Battillo <angela.battillo@gmail.com>
2021-07-11 15:08:58 -0600

    Add class names to card body for styles to come

--- src/Components/Cards/Cards.js
+++ src/Components/Cards/Cards.js
@@ -3,11 +3,17 @@ import React, { useState } from 'react';
 const Card = ({ firstName, lastName, street, unit, city, state, zip, speciality, phoneNum, key, vetted }) => {
   return (
       <article className='provider-card'>
-        <h2>{firstName}</h2>
-        <h2>{lastName}</h2>
-        <h3>{speciality}</h3>
-        <h3>{vetted}</h3>
-
+        <h2 className='first-name'>{firstName}</h2>
+        <h2 className='last-name'>{lastName}</h2>
+        <h3 className='specialty'>{speciality}</h3>
+        <h3 className='vetted'>{vetted}</h3>
+        <p className='address'>{street}
+           {unit}
+           {city}
+           {state}
+           {zip}
+           {phoneNum}
+        </p>
         <button className='accept-button'</button>
         <button className='deny-button'</button>
       </article>
