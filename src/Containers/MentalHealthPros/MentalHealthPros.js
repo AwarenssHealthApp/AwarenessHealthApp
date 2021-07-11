9bda793ac5e283b95b52cdb04bf6e1182cde5be3
Marceline Ball <marchettiball@gmail.com>
2021-07-10 21:16:27 -0600

    Refactor medntalhealthpros componet to take in new data format

--- src/Containers/MentalHealthPros/MentalHealthPros.js
+++ src/Containers/MentalHealthPros/MentalHealthPros.js
@@ -3,24 +3,24 @@ import { mentalHealthProfessionals } from '../../mockData'
 import Cards from '../../Components/Cards/Cards'
 
 const MentalHealthPros = () => {
-  const [mentalHealthProsList, setMentalHealthProsList] = useState(mentalHealthProfessionals)
+  const [mentalHealthProsList, setMentalHealthProsList] = useState(mentalHealthProfessionals.attributes.list)
   const allMentalHealthPros = mentalHealthProsList.map(mentalHealthPro => {
     return(
       <Cards
       id={mentalHealthPro.id}
       key={mentalHealthPro.id}
-      firstName={mentalHealthPro.attributes.first_name}
-      lastName={mentalHealthPro.attributes.last_name}
-      street={mentalHealthPro.attributes.street}
-      unit={mentalHealthPro.attributes.unit}
-      city={mentalHealthPro.attributes.city}
-      state={mentalHealthPro.attributes.state}
-      zip={mentalHealthPro.attributes.zip}
-      phone={mentalHealthPro.attributes.phone}
-      vetted={mentalHealthPro.attributes.vetted}
-      specialty={mentalHealthPro.attributes.specialty}
-      insurance={mentalHealthPro.attributes.insurance}
-      cost={mentalHealthPro.attributes.cost}
+      firstName={mentalHealthPro.first_name}
+      lastName={mentalHealthPro.last_name}
+      street={mentalHealthPro.street}
+      unit={mentalHealthPro.unit}
+      city={mentalHealthPro.city}
+      state={mentalHealthPro.state}
+      zip={mentalHealthPro.zip}
+      phone={mentalHealthPro.phone}
+      vetted={mentalHealthPro.vetted}
+      specialties={mentalHealthPro.specialties}
+      insurances={mentalHealthPro.insurances}
+      cost={mentalHealthPro.cost}
       />
     )
   })
