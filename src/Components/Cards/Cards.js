8b2d7f6bc73b72147e366f3b55895fa9e7c6bf3c
Angela Battillo <angela.battillo@gmail.com>
2021-07-11 15:07:40 -0600

    Start card body

--- src/Components/Cards/Cards.js
+++ src/Components/Cards/Cards.js
@@ -1,7 +1,17 @@
 import React, { useState } from 'react';
 
-const Card = ({ firstName, lastName, street, unit, city, state, zip, speciality, address, phoneNum, key, vetted }) => {
+const Card = ({ firstName, lastName, street, unit, city, state, zip, speciality, phoneNum, key, vetted }) => {
+  return (
+      <article className='provider-card'>
+        <h2>{firstName}</h2>
+        <h2>{lastName}</h2>
+        <h3>{speciality}</h3>
+        <h3>{vetted}</h3>
 
+        <button className='accept-button'</button>
+        <button className='deny-button'</button>
+      </article>
+    )
 
 }
 
