e8780b97a126fb23b4e871a5e9eb9e6ffe359693
Riley Martin <riley.willow@gmail.com>
2021-07-13 16:06:22 -0600

    Add ThemeContextProvider to App

--- src/Containers/App/App.js
+++ src/Containers/App/App.js
@@ -3,6 +3,7 @@ import ContributionForm from '../ContributionForm/ContributionForm';
 import Doctors from '../Doctors/Doctors';
 import MentalHealthPros from '../MentalHealthPros/MentalHealthPros';
 import { Route, Switch, Link } from 'react-router-dom';
+import ThemeContextProvider from '../../contexts/ThemeContext';
 import './_app.scss';
 import background from '../../assets/images/peacefulOceanImage.png';
 
@@ -11,44 +12,46 @@ function App() {
   return (
     <div className='App' style={{
             backgroundImage: `url(${background})`}}>
-      <header >
-        <h1 className='App-title'>Head To Toe</h1>
-        <nav className='nav-bar'>
-          <Link to='/doctors'>
-            <button className='App-buttons' id='doctors-button'>Doctors</button>
-          </Link>
-          <Link to='/mental_health_professionals'>
-            <button className='App-buttons' id='mental-health-pros-button'>Mental Health Professionals</button>
-          </Link>
-          <Link to='/submit'>
-            <button className='App-buttons' id='submit-button'>Submit</button>
-          </Link>
-        </nav>
-      </header>
-      <Switch>
-        <Route path='/submit' render={() => {
-          return(
-            <ContributionForm />
-          )
-        }} />
-        <Route path='/doctors' render={() => {
-          return(
-            <Doctors />
-          )
-        }} />
-        <Route path='/mental_health_professionals' render={() => {
-          return(
-            <MentalHealthPros />
-          )
-        }} />
-        <Route exact path ='/' render={() => {
-          return(
-            <main>
-              <p className='mission'>A place to find queer affirming support</p>
-            </main>
-          )
-        }} />
-      </Switch>
+      <ThemeContextProvider>
+        <header >
+          <h1 className='App-title'>Head To Toe</h1>
+          <nav className='nav-bar'>
+            <Link to='/doctors'>
+              <button className='App-buttons' id='doctors-button'>Doctors</button>
+            </Link>
+            <Link to='/mental_health_professionals'>
+              <button className='App-buttons' id='mental-health-pros-button'>Mental Health Professionals</button>
+            </Link>
+            <Link to='/submit'>
+              <button className='App-buttons' id='submit-button'>Submit</button>
+            </Link>
+          </nav>
+        </header>
+        <Switch>
+          <Route path='/submit' render={() => {
+            return(
+              <ContributionForm />
+            )
+          }} />
+          <Route path='/doctors' render={() => {
+            return(
+              <Doctors />
+            )
+          }} />
+          <Route path='/mental_health_professionals' render={() => {
+            return(
+              <MentalHealthPros />
+            )
+          }} />
+          <Route exact path ='/' render={() => {
+            return(
+              <main>
+                <p className='mission'>A place to find queer affirming support</p>
+              </main>
+            )
+          }} />
+        </Switch>
+      </ThemeContextProvider>
     </div>
   );
 }
