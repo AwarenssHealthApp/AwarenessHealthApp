8e6950d5a01d44e005ffb8cb7ee7b04a8f321975
Riley Martin <riley.willow@gmail.com>
2021-07-13 15:50:05 -0600

    Write toggleDarkMode method

--- src/contexts/ThemeContext.js
+++ src/contexts/ThemeContext.js
@@ -5,6 +5,9 @@ export const ThemeContext = createContext()
 const ThemeContextProvider = (props) => {
   const [darkMode, setDarkMode] = useState(false)
 
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode)
+  }
 }
 
 export default ThemeContextProvider
