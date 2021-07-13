e868dcf6452b97a996efe325e4943acb434f9a6d
Angela Battillo <angela.battillo@gmail.com>
2021-07-12 21:15:34 -0600

    Add appropriate intercept for stubbing data

--- cypress/integration/ContributionForm_spec.js
+++ cypress/integration/ContributionForm_spec.js
@@ -1,31 +1,54 @@
-describe('ContributionForm', () => {
-  beforeEach(() => {
-    cy.visit('http://localhost:3000/submit')
-  })
+describe('ContributionForm View', () => {
+
+    beforeEach(() => {
+      cy.fixture('mockDr.json')
+        .then(mockData => {
+          cy.intercept('GET', 'https://head-to-toe-be.herokuapp.com/api/v1/medical_professionals?type=doctor&state=South Dakota', {
+            statusCode: 201,
+            delay: 100,
+            body: mockData
+          })
+        })
+      cy.visit('http://localhost:3000/submit')
+    });
+
   it('Should have a controlled input field for first name whose value reflects the data typed into the form', () => {
     cy.get('form input[name="firstName"]').type('Betty')
       .get('form input[name="firstName"]').should('have.value', 'Betty')
-  })
+  });
+
   it('Should have a controlled input field for last name whose value reflects the data typed into the form', () => {
     cy.get('form input[name="lastName"]').type('White')
       .get('form input[name="lastName"]').should('have.value', 'White')
   })
+
   it('Should have a controlled input field for specialty whose value reflects the data typed into the form', () => {
     cy.get('form input[name="specialty"]').type('trans health')
       .get('form input[name="specialty"]').should('have.value', 'trans health')
-  })
+  });
+
   it('Should have a controlled input field for insurance whose value reflects the data typed into the form', () => {
     cy.get('form input[name="insurance"]').type('sliding scale')
       .get('form input[name="insurance"]').should('have.value', 'sliding scale')
-  })
+  });
+
   it('Should have a controlled input field for address whose value reflects the data typed into the form', () => {
     cy.get('form input[name="address"]').type('123 Main St')
       .get('form input[name="address"]').should('have.value', '123 Main St')
-  })
+  });
+
   it('Should have a controlled input field for phone number whose value reflects the data typed into the form', () => {
     cy.get('form input[name="phoneNum"]').type('1-800-GOOD-DOC')
       .get('form input[name="phoneNum"]').should('have.value', '1-800-GOOD-DOC')
-  })
+  });
+
+  it('should display the sumbit page when the sumbit button is pressed', () => {
+    cy.get('#submit-button').click()
+      .location('pathname').should('eq', '/submit')
+      .get('h2').should('contain', 'Contribution Form')
+      .get('form input[name="firstName"]')
+  });
+
   // describe('ContributionForm Error Handling', () => {
   //   beforeEach(() => {
   //     cy.get('form input[name="firstName"]').type('Betty')
@@ -46,4 +69,4 @@ describe('ContributionForm', () => {
   //     .get('.confirm-msg').should('have.text', 'Thank you for your suggestion! We will review your submission for approval.')
   //   })
   // })
-})
+});
--- cypress/integration/router_spec.js
+++ cypress/integration/router_spec.js
@@ -1,50 +1,58 @@
-describe('router', () => {
-  beforeEach(() => {
-    cy.visit('http://localhost:3000')
-  })
+describe('Router Flow', () => {
+
+    beforeEach(() => {
+      cy.fixture('mockDr.json')
+        .then(mockData => {
+          cy.intercept('GET', 'https://head-to-toe-be.herokuapp.com/api/v1/medical_professionals?type=doctor&state=South Dakota', {
+            statusCode: 201,
+            delay: 100,
+            body: mockData
+          })
+        })
+      cy.visit('http://localhost:3000/')
+    });
 
   it('should have a url of "/"', () => {
     cy.location('pathname').should('eq', '/')
-  })
+  });
 
   it('should display the title on the main page', () => {
     cy.get('h1').should('contain', 'Head To Toe')
-  })
+  });
 
   it('should display the navigation buttons on the main page', () => {
     cy.get('#doctors-button').should('contain', 'Doctors')
       .get('#mental-health-pros-button').should('contain', 'Mental Health Professionals')
       .get('#submit-button').should('contain', 'Submit')
-  })
-
+  });
 
   it('should display the doctors page when the doctor button is pressed', () => {
     cy.get('#doctors-button').click()
       .location('pathname').should('eq', '/doctors')
       .get('h2').last().should('contain', 'Doctors here')
-  })
+  });
 
   it('should return to the main page when the back button is clicked', () => {
     cy.get('#doctors-button').click()
       .location('pathname').should('eq', '/doctors')
       .go('back')
       .location('pathname').should('eq', '/')
-  })
+  });
 
   it('should navigate to the mental health pro page when the mental health pro button is pressed', () => {
     cy.get('#mental-health-pros-button').click()
       .location('pathname').should('eq', '/mental_health_professionals')
-  })
+  });
 
   it('should return to the main page when the back button is clicked', () => {
     cy.get('#mental-health-pros-button').click()
       .location('pathname').should('eq', '/mental_health_professionals')
       .go('back')
       .location('pathname').should('eq', '/')
-  })
+  });
 
   it('should display the sumbit page when the sumbit button is pressed', () => {
     cy.get('#submit-button').click()
       .location('pathname').should('eq', '/submit')
-  })
-})
+  });
+});
