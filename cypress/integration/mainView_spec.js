b2bb74353e9ac7cc359ab479159901f0542fa115
Angela Battillo <angela.battillo@gmail.com>
2021-07-12 20:01:03 -0600

    Create initial main page stubbing before each and verify page opens

--- cypress/integration/mainView_spec.js
+++ cypress/integration/mainView_spec.js
@@ -1,1 +1,23 @@
+describe('Show landing page of Head To Toe', () => {
 
+  beforeEach(() => {
+    cy.fixture('mockDr.json')
+      .then(mockData => {
+        cy.intercept('GET', 'https://ron-swanson-quotes.herokuapp.com/v2/quotes', {
+          statusCode: 201,
+          delay: 100,
+          body: mockData
+        })
+      })
+    cy.visit('http://localhost:3000/')
+  });
+
+  it('Should be able to open to the main page', () => {
+  cy.url().should('eq', 'http://localhost:3000/')
+  });
+
+it('should display the title on the main page', () => {
+  cy.get('h1').should('contain', 'Head To Toe')
+  });
+
+})
