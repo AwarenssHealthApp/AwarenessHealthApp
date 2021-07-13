5aa3e254d3eb3a7cd210f62f953653166ee33464
Riley Martin <riley.willow@gmail.com>
2021-07-13 15:56:47 -0600

    Establish color objects

--- src/contexts/ThemeContext.js
+++ src/contexts/ThemeContext.js
@@ -3,6 +3,14 @@ import React, { createContext, useState } from 'react'
 export const ThemeContext = createContext()
 
 const ThemeContextProvider = (props) => {
+  const [light, setLight] = useState({
+    background: white,
+    color: black
+  })
+  const [dark, setDark] = useState({
+    background: black,
+    color: white
+  })
   const [darkMode, setDarkMode] = useState(false)
 
   const toggleDarkMode = () => {
