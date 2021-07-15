80dd4f948970ecf3ed8cda91b970614ffdcdd610
Marceline Ball <marchettiball@gmail.com>
2021-07-14 18:05:43 -0600

    Create search input

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -1,7 +1,20 @@
 import React, { useState } from 'react'
 
 const SearchBar = () => {
+  const [searchInput, setSearchInput] = useState('')
 
+  return(
+    <form>
+      <input
+        type='text'
+        placeholder='Search'
+        name='search'
+        value={searchInput}
+        onChange={event => setSearchInput(event.target.value)}
+      />
+
+    </form>
+  )
 }
 
 export default SearchBar
