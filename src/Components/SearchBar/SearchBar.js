430dd64caf1b538413caef0a42180fa050362228
Marceline Ball <marchettiball@gmail.com>
2021-07-14 20:45:25 -0600

    Add filterDoctors method to SearchBar

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -1,13 +1,21 @@
 import React, { useState } from 'react'
 
-const SearchBar = ({  }) => {
+const SearchBar = ({ doctorList, setFilteredDoctorList }) => {
   const [searchInput, setSearchInput] = useState('')
+
+  const filterDoctors = (doctorList) => {
+    const filteredDoctors = doctorList.filter(doctor => {
+      return doctor.insurances.filter(insurance => {
+        return insurance.company.toUpperCase().includes(searchInput.toUpperCase)
+      })
+    })
+  }
 
   return(
     <form>
       <input
         type='text'
-        placeholder='Search'
+        placeholder='Search by Insurance'
         name='search'
         value={searchInput}
         onChange={event => setSearchInput(event.target.value)}
