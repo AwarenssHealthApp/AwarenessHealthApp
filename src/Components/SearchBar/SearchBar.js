b5379e71eefec95c51374797503c7f71ee8504aa
Marceline Ball <marchettiball@gmail.com>
2021-07-14 20:56:24 -0600

    Add handleSearch function to SearchBar

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -3,12 +3,19 @@ import React, { useState } from 'react'
 const SearchBar = ({ doctorList, setFilteredDoctorList }) => {
   const [searchInput, setSearchInput] = useState('')
 
-  const filterDoctors = (doctorList) => {
+  const handleSearch = (event) => {
+    event.preventDefault()
+    const searchResults = filterDoctors()
+    setFilteredDoctorList(searchResults)
+  }
+
+  const filterDoctors = () => {
     const filteredDoctors = doctorList.filter(doctor => {
       return doctor.insurances.filter(insurance => {
         return insurance.company.toUpperCase().includes(searchInput.toUpperCase)
       })
     })
+    return filteredDoctors
   }
 
   return(
@@ -20,7 +27,7 @@ const SearchBar = ({ doctorList, setFilteredDoctorList }) => {
         value={searchInput}
         onChange={event => setSearchInput(event.target.value)}
       />
-
+      <button onClick={(event) =>  handleSearch(event)}>Submit</button>
     </form>
   )
 }
