73aee75322ea580c39654922f70bab10ad2e20dd
Riley Martin <riley.willow@gmail.com>
2021-07-17 10:32:31 -0600

    Remove unnecessary code

--- src/Containers/ContributionForm/ContributionForm.js
+++ src/Containers/ContributionForm/ContributionForm.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from 'react';
+import React, { useState } from 'react';
 import { DropDown } from '../../Components/DropDown/DropDown'
 import { addContribution } from '../../utils/apiCalls'
 
