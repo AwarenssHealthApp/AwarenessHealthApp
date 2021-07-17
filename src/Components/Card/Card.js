eeeed60b4ab804686a5114bea09d55f452e56818
Marceline Ball <marchettiball@gmail.com>
2021-07-17 16:50:43 -0600

    Refactor function to take in either specialties or insurance to return seperated by commas

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -3,24 +3,26 @@ import PropTypes from 'prop-types';
 import './_card.scss';
 
 const Card = ({ firstName, lastName, street, unit, city, state, zip, specialties, phone, insurances }) => {
-  const allSpecialties = () => {
-    let tempSpecialties = []
-    specialties.forEach(specialty => {
-      tempSpecialties.push(specialty.name)
+  const allListItems = (type) => {
+    let tempListItems = []
+    type.forEach(listItem => {
+      if (type === specialties) {
+        tempListItems.push(listItem.name)
+      } else if (type === insurances) {
+        tempListItems.push(listItem.company)
+      }
     })
-    return tempSpecialties.join(', ')
+    return tempListItems.join(', ')
   }
-  const allInsurances = insurances.map(insurance => {
-    return insurance.company
-  })
+
   return (
     <section className='provider-card-wrapper'>
       <article className='provider-card'>
         <h2 className='first-name'>{firstName}</h2>
         <h2 className='last-name'>{lastName}</h2>
-        <h3 className='specialties'>Specializes in {allSpecialties()}</h3>
+        <h3 className='specialties'>Specializes in {allListItems(specialties)}</h3>
         <p className='address'>{street} {unit} {city}, {state} {zip} {phone}</p>
-        <p className='insurances'>Accepts {allInsurances}</p>
+        <p className='insurances'>Accepts {allListItems(insurances)}</p>
         <button className='accept-button'>Love</button>
         <button className='deny-button'>Don't Love</button>
       </article>
