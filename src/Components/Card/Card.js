6b093a1ac05c209a70117cf4b419db7f8cb2ec68
Marceline Ball <marchettiball@gmail.com>
2021-07-17 13:24:38 -0600

    Add specialities to cards

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import './_card.scss';
 
-const Card = ({ firstName, lastName, street, unit, city, state, zip, specialties, phone, vetted }) => {
+const Card = ({ firstName, lastName, street, unit, city, state, zip, specialties, phone, insurances }) => {
   const allSpecialties = specialties.map(specialty => {
-    return specialty.name + ' '
+    return specialty.name + ', '
+  })
+  const allInsurances = insurances.map(insurance => {
+    return insurance.company + ', '
   })
   return (
     <section className='provider-card-wrapper'>
       <article className='provider-card'>
         <h2 className='first-name'>{firstName}</h2>
         <h2 className='last-name'>{lastName}</h2>
-        <h3 className='specialties'>{allSpecialties}</h3>
+        <h3 className='specialties'>Specializes in {allSpecialties}</h3>
         <p className='address'>{street} {unit} {city}, {state} {zip} {phone}</p>
+        <p className='insurances'>Accepts {allInsurances}</p>
         <button className='accept-button'>Love</button>
         <button className='deny-button'>Don't Love</button>
       </article>
