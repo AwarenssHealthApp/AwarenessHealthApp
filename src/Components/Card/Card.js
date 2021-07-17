140e60cc4659af977cf9f173a3f04767d1fff8ad
Marceline Ball <marchettiball@gmail.com>
2021-07-17 13:16:15 -0600

    Refactor card to display specialities and spaceing

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,20 +1,17 @@
 import React from 'react';
 import './_card.scss';
 
-const Card = ({ firstName, lastName, street, unit, city, state, zip, speciality, phoneNum, vetted }) => {
+const Card = ({ firstName, lastName, street, unit, city, state, zip, specialties, phone, vetted }) => {
+  const allSpecialties = specialties.map(specialty => {
+    return specialty.name + ' '
+  })
   return (
     <section className='provider-card-wrapper'>
       <article className='provider-card'>
         <h2 className='first-name'>{firstName}</h2>
         <h2 className='last-name'>{lastName}</h2>
-        <h3 className='specialty'>{speciality}</h3>
-        <p className='address'>{street}
-           {unit}
-           {city}
-           {state}
-           {zip}
-           {phoneNum}
-        </p>
+        <h3 className='specialties'>{allSpecialties}</h3>
+        <p className='address'>{street} {unit} {city}, {state} {zip} {phone}</p>
         <button className='accept-button'>Love</button>
         <button className='deny-button'>Don't Love</button>
       </article>
