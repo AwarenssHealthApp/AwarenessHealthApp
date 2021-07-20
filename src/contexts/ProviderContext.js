91bea0318935ef3bdf4bfb469648c530a2aff3f1
Angela Battillo <angela.battillo@gmail.com>
2021-07-19 22:02:39 -0600

    Start new Context for provider lists coming in from api's

---
+++ src/contexts/ProviderContext.js
@@ -0,0 +1,13 @@
+import React, { createContext, useState } from 'react';
+
+
+export const ProviderContextProvider = (props) => {
+
+  const [doctorList, setDoctorList] = useState([])
+  const [filteredDoctorList, setFilteredDoctorList] = useState([])
+
+  const [mentalHealthProsList, setMentalHealthProsList] = useState([])
+  const [filteredMentalHealthProsList, setFilteredMentalHealthProsList] = useState([])
+
+
+}
