c7b794bb8569beb4b2bc93b1554a2e716db59b1e
Angela Battillo <angela.battillo@gmail.com>
2021-07-19 22:03:37 -0600

    Fill in Context for provider lists

--- src/contexts/ProviderContext.js
+++ src/contexts/ProviderContext.js
@@ -1,5 +1,14 @@
 import React, { createContext, useState } from 'react';
-
+export const ProviderContext = createContext({
+  doctorList: () => {},
+  setDoctorList: () => {},
+  filteredDoctorList: () => {},
+  setFilteredDoctorList: () => {},
+  mentalHealthProsList:  () => {},
+  setMentalHealthProsList: () => {},
+  filteredMentalHealthProsList: () => {},
+  setFilteredMentalHealthProsList: () => {}
+});
 
 export const ProviderContextProvider = (props) => {
 
@@ -9,5 +18,20 @@ export const ProviderContextProvider = (props) => {
   const [mentalHealthProsList, setMentalHealthProsList] = useState([])
   const [filteredMentalHealthProsList, setFilteredMentalHealthProsList] = useState([])
 
-
+  return (
+    <ProviderContext.Provider
+      value={{
+        doctorList,
+        setDoctorList,
+        filteredDoctorList,
+        setFilteredDoctorList,
+        mentalHealthProsList,
+        setMentalHealthProsList,
+        filteredMentalHealthProsList,
+        setFilteredMentalHealthProsList
+      }}
+    >
+      {props.children}
+    </ProviderContext.Provider>
+  )
 }
