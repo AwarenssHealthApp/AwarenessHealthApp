e4f6db0ab4d50679c675fea7c6b57fed8ed11a17
Riley Martin <riley.willow@gmail.com>
2021-08-19 10:00:05 -0600

    Create Admin Component

---
+++ src/Containers/Admin/Admin.js
@@ -0,0 +1,25 @@
+import React from 'react';
+//import API calls from '../../utils/apiCalls';
+import Card from '../../Components/Card/Card';
+import Error from '../../Components/Error/Error';
+import './_admin.scss';
+
+const Admin = () => {
+  //hooks for unvetted provider data
+
+  //useEffect for fetching unvetted providers list and setting state
+
+  //method to approve suggestion
+
+  //method to delete suggestion
+
+  //iterate over unvetted list to create cards
+    //update Card component to implement conditional rendering to display approve and delete buttons IF vetted = false
+
+  //return all cards
+
+}
+
+//implement propTypes
+
+export default Admin
