c11f5e79c68784f9239b8f10498f986be280adca
Riley Martin <riley.willow@gmail.com>
2021-08-19 10:20:47 -0600

    Implement ThemeContext for light and dark mode

--- src/Containers/AdminApproval/AdminApproval.js
+++ src/Containers/AdminApproval/AdminApproval.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 //import API calls from '../../utils/apiCalls';
 import Card from '../../Components/Card/Card';
+import { ThemeContext } from '../../contexts/ThemeContext';
 import Error from '../../Components/Error/Error';
-import './_admin.scss';
+import './_adminApproval.scss';
 
 const Admin = () => {
   //hooks for unvetted provider data
+  const { darkMode, light, dark } = useContext(ThemeContext);
+  const theme = darkMode ? dark : light;
 
   //useEffect for fetching unvetted providers list and setting state
 
@@ -17,6 +20,12 @@ const Admin = () => {
     //update Card component to implement conditional rendering to display approve and delete buttons IF vetted = false
 
   //return all cards
+  return (
+    <div className={'theme ' + (dark ? 'theme--dark' : 'theme--default')}
+      style={{ color: theme.color, background: theme.background }}>
+      <h1>Admin Approval Page</h1>
+    </div>
+  )
 
 }
 
