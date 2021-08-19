f393686f01d332d6df97126f3c900944b75be6d4
Riley Martin <riley.willow@gmail.com>
2021-08-19 10:49:42 -0600

    Fix syntax errors in test file

--- cypress/integration/AdminApproval_spec.js
+++ cypress/integration/AdminApproval_spec.js
@@ -1,19 +1,19 @@
 describe('AdminApproval', () => {
-  // beforeEach(() => {
-  //   cy.fixture('mockDr.json')
-  //     .then(mockData => {
-  //       cy.intercept('GET', 'https://head-to-toe-be.herokuapp.com/api/v1/medical_professionals?vetted=false', {
-  //         statusCode: 201,
-  //         delay: 100,
-  //         body: mockData
-  //       })
-  //     })
-  //   cy.visit('http://localhost:3000/admin_approval_who_dis')
-  // })
+  beforeEach(() => {
+    // cy.fixture('mockDr.json')
+    //   .then(mockData => {
+    //     cy.intercept('GET', 'https://head-to-toe-be.herokuapp.com/api/v1/medical_professionals?vetted=false', {
+    //       statusCode: 201,
+    //       delay: 100,
+    //       body: mockData
+    //     })
+    //   })
+    cy.visit('http://localhost:3000/admin_approval_who_dis')
+  })
   it('Should display the page title', () => {
-    .get('.admin-sub-title').should('be.visible')
-    .get('.admin-sub-title').should('contain', 'Admin Approval')
-    .get('.admin-sub-title').should('have.css', 'font-family', 'opendyslexic')
+    cy.get('.admin-sub-title').should('be.visible')
+      .get('.admin-sub-title').should('contain', 'Admin Approval')
+      .get('.admin-sub-title').should('have.css', 'font-family', 'opendyslexic')
   })
   it('Should display unvetted healthcare providers', () => {
 
