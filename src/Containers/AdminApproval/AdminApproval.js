00d1187443a39ab8f9e5766c0767d05f81f357e4
Riley Martin <riley.willow@gmail.com>
2021-08-20 10:31:10 -0600

    Implement GET request for unvetted providers in AdminApproval

--- src/Containers/AdminApproval/AdminApproval.js
+++ src/Containers/AdminApproval/AdminApproval.js
@@ -1,17 +1,31 @@
-import React, { useContext } from 'react';
-//import API calls from '../../utils/apiCalls';
+import React, { useState, useEffect, useContext } from 'react';
+import { retrieveUnvetted } from '../../utils/apiCalls';
 // import Card from '../../Components/Card/Card';
 import { ThemeContext } from '../../contexts/ThemeContext';
 // import Error from '../../Components/Error/Error';
 import './_adminApproval.scss';
 
 const Admin = () => {
-  //hooks for unvetted provider data
+  const [unvettedList, setUnvettedList] = useState([])
+  const [error, setError] = useState('')
   const { darkMode, light, dark } = useContext(ThemeContext);
   const theme = darkMode ? dark : light;
 
-  //useEffect for fetching unvetted providers list and setting state
+  useEffect(() => {
+    let mounted = true;
+    retrieveUnvetted()
+      .then(providers => {
+        if(mounted) {
+          setUnvettedList([...providers.data.attributes.doctors, ...providers.data.attributes.mhps])
+        }
+      })
+      .catch(error => setError('Oops, looks like our computer gnome is fixing something right now.  Please try again later'))
+    return () => mounted = false;
+  }, [setUnvettedList])
 
+  const handleClick = () => {
+    console.log(unvettedList)
+  }
   //method to approve suggestion
 
   //method to delete suggestion
@@ -24,6 +38,7 @@ const Admin = () => {
     <div className={'theme ' + (dark ? 'theme--dark' : 'theme--default')}
       style={{ color: theme.color, background: theme.background }}>
       <h2 className='admin-sub-title'>Admin Approval</h2>
+      <button onClick={handleClick}>check fetch</button>
     </div>
   )
 
