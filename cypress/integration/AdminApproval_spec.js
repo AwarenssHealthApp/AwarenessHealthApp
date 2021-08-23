9b580fd8ed124f9ff179f909180e131666a31657
Marceline Ball <marchettiball@gmail.com>
2021-08-23 09:43:08 -0600

    Merge rebase conflict

--- cypress/integration/AdminApproval_spec.js
+++ cypress/integration/AdminApproval_spec.js
@@ -10,11 +10,13 @@ describe('AdminApproval', () => {
       })
     cy.visit('http://localhost:3000/admin_approval_who_dis')
   })
+
   it('Should display the page title', () => {
     cy.get('.admin-sub-title').should('be.visible')
       .get('.admin-sub-title').should('contain', 'Admin Approval')
       .get('.admin-sub-title').should('have.css', 'font-family', 'opendyslexic')
   })
+
   it('Should display unvetted healthcare providers', () => {
     cy.get('.provider-card').should('be.visible')
       .get('.first-name').should('be.visible')
@@ -25,14 +27,21 @@ describe('AdminApproval', () => {
       .get('.insurances').should('contain', 'Schinner, Kreiger and Stokes, Yundt-Cronin, Herman-Bogan')
       .get('.all-unvetted').should('have.length', 1)
   })
+
   it('Should display an approve and deny button for each card', () => {
     cy.get('.approve-button').should('be.visible')
       .get('.deny-button').should('be.visible')
   })
+
   it('Should display a message stating there are no unvetted suggestions if all providers are vetted', () => {
 
   })
+
   it('Should display an error message when there is an error thrown in the fetch request', () => {
 
+  })
+
+  it('Should make a patch request when the user clicks the approve button', () => {
+    
   })
 })
