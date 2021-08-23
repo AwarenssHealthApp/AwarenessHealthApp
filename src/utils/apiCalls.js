5e97c9115ee8681677db9ddb2af3a59aff2aec6f
Marceline Ball <marchettiball@gmail.com>
2021-08-23 09:40:50 -0600

    Merge rebase conflict

--- src/utils/apiCalls.js
+++ src/utils/apiCalls.js
@@ -40,7 +40,7 @@ const retrieveUnvetted = () => {
 }
 
 const approveContribution = (id, profession) => {
-  return fetch(`https://head-to-toe-be.herokuapp.com/api/v1/medical_professionals/${id}`, {
+  return fetch(`https://head-to-toe-be.herokuapp.com/api/v1/medical_professionals/${id}`), {
     method: 'PATCH',
     headers: {
       'api-key': 'aidanisthebest',
